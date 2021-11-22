ce2ce6aff5c50b8ed90d2bc434c2dc72598463ed
Buddha <yarwol24@gmail.com>
2021-11-22 18:23:21 +0100

    Answer to first day (b)

--- 2015/Q1/main.py
+++ 2015/Q1/main.py
@@ -1,12 +1,20 @@
-
 floor = 0
-with open('instruction.txt') as fp:
-    for char in fp.readline():
-        if char == '(':
+with open('Q1/instruction.txt') as fp:
+    basement = True
+    characters = fp.readline()
+    for i in range(len(characters)):
+        if characters[i] == '(':
             floor += 1
-        elif char == ')':
+        elif characters[i] == ')':
             floor -= 1
         else:
-            print('unknown character {}'.format(char))
+            print('unknown character {}'.format(characters[i]))
 
-print("Santa is on floor: {}".format(floor))
+        if floor == -1 and basement:
+            basement = False
+            first_time_entering_basement = i + 1    # Account for zeroindex
+
+
+
+print("Ans Q1a::   Santa is on floor: {}".format(floor))
+print("Ans Q1b::   Santa enters the basement at index: {}".format(first_time_entering_basement))
