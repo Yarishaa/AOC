d0e69273de9f33e9537d80258e69ff69ac1d57d5
Buddha <yarwol24@gmail.com>
2021-12-04 13:22:52 +0100

    solution to q1 usin python

---
+++ Python/2021/Q1/main.py
@@ -0,0 +1,46 @@
+
+def get_depth_values(filename):
+    datapoint = []
+
+    try:
+        with open(filename) as fp:
+            for val in fp.readlines():
+                datapoint.append(int(val.split('\n')[0]))
+
+    except FileNotFoundError():
+        raise FileNotFoundError('Cannot find specified file!')
+
+    finally:
+        return datapoint
+
+
+def question_1a(measurement_data):
+    increased_counter = 0
+    for idx in range((len(measurement_data)-1)):
+        if measurement_data[idx + 1] > measurement_data[idx]:
+            increased_counter += 1
+
+    print("Ans Q1a:: The depth increased {} times".format(increased_counter))
+
+
+def question_1b(measurement_data):
+    increased_counter = 0
+    for idx in range((len(measurement_data) - 3)):
+        sum1 = measurement_data[idx] + measurement_data[idx + 1] + measurement_data[idx + 2]
+        sum2 = measurement_data[idx + 1] + measurement_data[idx + 2] + measurement_data[idx + 3]
+
+        if sum2 > sum1:
+            increased_counter += 1
+
+    print("Ans Q1b:: The number of times the following sum increased is {}".format(increased_counter))
+
+
+if __name__ == '__main__':
+    file = '../instructions/oceanDepth'
+    data = get_depth_values(file)
+
+    # solve for question 1a
+    question_1a(data)
+
+    # solve for question 1b
+    question_1b(data)
